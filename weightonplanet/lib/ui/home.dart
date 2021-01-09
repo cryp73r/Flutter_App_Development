a49912d864d6443efbfa010766b76b6ed1d26b14
Priyanshu Singh <priyanshusingh0610@gmail.com>
2021-01-09 23:10:02 +0530

    Weight on Planet X

--- weightonplanet/lib/ui/home.dart
+++ weightonplanet/lib/ui/home.dart
@@ -6,6 +6,16 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int radioValue = 0;
+
+  void handleRadioValueChanged(int value) {
+    setState(() {
+      radioValue = value;
+
+      print(radioValue);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,6 +47,51 @@ class _HomeState extends State<Home> {
                         labelText: 'Your Weight',
                         hintText: 'In Pounds',
                         icon: Icon(Icons.person_outline)
+                    ),
+                  ),
+                  new Padding(padding: new EdgeInsets.all(5.0)),
+                //  three new Radio Button
+                  new Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      new Radio<int>(
+                        activeColor: Colors.brown,
+                          value: 0, groupValue: radioValue, onChanged: handleRadioValueChanged),
+
+                      new Text(
+                        'Pluto',
+                        style: TextStyle(color: Colors.white30),
+                      ),
+
+                      new Radio<int>(
+                        activeColor: Colors.redAccent,
+                          value: 1, groupValue: radioValue, onChanged: handleRadioValueChanged),
+
+                      new Text(
+                        'Mars',
+                        style: TextStyle(color: Colors.white30),
+                      ),
+
+                      new Radio<int>(
+                        activeColor: Colors.orangeAccent,
+                          value: 2, groupValue: radioValue, onChanged: handleRadioValueChanged),
+
+                      new Text(
+                        'Venus',
+                        style: TextStyle(color: Colors.white30),
+                      )
+                    ],
+                  ),
+                //  Result text
+                  Padding(
+                    padding: const EdgeInsets.all(15.6),
+                    child: new Text(
+                      'Hello There',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 19.4,
+                        fontWeight: FontWeight.w500
+                      ),
                     ),
                   )
                 ],
