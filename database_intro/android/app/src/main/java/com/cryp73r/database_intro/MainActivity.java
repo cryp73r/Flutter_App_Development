606139da2d13167d6be2dba8c09bf0668a799eec
Priyanshu Singh <priyanshusingh0610@gmail.com>
2021-01-25 01:54:05 +0530

    Database Introduction

---
+++ database_intro/android/app/src/main/java/com/cryp73r/database_intro/MainActivity.java
@@ -0,0 +1,6 @@
+package com.cryp73r.database_intro;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ database_intro/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: "Database",
+      home: Home(),
+    )
+  );
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Database"),
+        centerTitle: true,
+      ),
+    );
+  }
+}
