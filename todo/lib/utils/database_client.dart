38f117ec20cc487cb669b5926727cca3234e826a
Priyanshu Singh <priyanshusingh0610@gmail.com>
2021-04-03 14:44:23 +0530

    ToDo List

--- todo/lib/utils/database_client.dart
+++ todo/lib/utils/database_client.dart
@@ -3,15 +3,87 @@ import 'dart:async';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:todo/models/todo_item.dart';
+import 'package:todo/models/todo_item.dart';
 
 class DatabaseHelper {
   static final DatabaseHelper _instance = DatabaseHelper.internal();
   factory DatabaseHelper() => _instance;
 
   final String tableName = "ToDoTbl";
-  final String ColumnId = "id";
+  final String columnId = "id";
   final String columnItemName = "itemName";
   final String columnDateCreated = "dateCreated";
 
   static Database _db;
+
+  Future<Database> get db async {
+    if (_db!=null) {
+      return _db;
+    }
+    _db = await initDb();
+    return _db;
+  }
+
+  DatabaseHelper.internal();
+
+  initDb() async {
+    Directory documentDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentDirectory.path, "todo_db.db");
+    var ourDb = await openDatabase(path, version: 1, onCreate: _onCreate);
+    return ourDb;
+  }
+  
+  void _onCreate(Database db, int version) async {
+    await db.execute(
+      "CREATE TABLE $tableName(id INTEGER PRIMARY KEY, $columnItemName TEXT, $columnDateCreated TEXT)"
+    );
+    print("Table is Created");
+  }
+
+//  Insertion
+  Future<int> saveItem(ToDoItem item) async {
+    var dbClient = await db;
+    int res = await dbClient.insert("$tableName", item.toMap());
+    print(res.toString());
+    return res;
+  }
+
+//  Get
+Future<List> getItems() async {
+    var dbClient = await db;
+    var result = await dbClient.rawQuery("SELECT * FROM $tableName ORDER BY $columnItemName ASC");
+    return result.toList();
+}
+
+Future<int> getCount() async {
+    var dbClient = await db;
+    return Sqflite.firstIntValue(await dbClient.rawQuery(
+      "SELECT COUNT(*) FROM $tableName"
+    ));
+}
+
+Future<ToDoItem> getItem(int id) async {
+    var dbClient = await db;
+    var result = await dbClient.rawQuery("SELECT * FROM $tableName WHERE id = $id");
+    if (result.length==0) return null;
+    return ToDoItem.fromMap(result.first);
+}
+
+Future<int> deleteItem(int id) async {
+    var dbClient = await db;
+    return await dbClient.delete(tableName,
+    where: "$columnId = ?", whereArgs: [id]);
+}
+
+Future<int> updateItem(ToDoItem item) async {
+    var dbClient = await db;
+    return await dbClient.update("$tableName", item.toMap(),
+    where: "$columnId = ?", whereArgs: [item.id]);
+}
+
+Future close() async {
+    var dbClient = await db;
+    return dbClient.close();
+}
 }
