debd6678bb8d6c6e28ec355ca6887870cac6b748
Priyanshu Singh <priyanshusingh0610@gmail.com>
2021-04-24 22:59:17 +0530

    UFC

--- udemy_free_courses/lib/utils/rQuery.dart
+++ udemy_free_courses/lib/utils/rQuery.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 
 Future<Map> getQuery() async {
   var apiUrl = Uri.https("www.udemy.com", "/api-2.0/courses/");
-  http.Response response = await http.get(apiUrl, headers: {"Authorization": "Basic SGQwQXNOTTg2NmtrUFNFOUhYcTk2VHM5SjhTbnNWRllwM2FEcW5ZTTppWmtKd042WEp6VzN5bXRRSHJoMk1IZVhValJnTElrMDRUQ2Q5UHhwanRtTlJ5SnlNdDdmSUJyOVh6emdBenBVaXlrcFZmdGJrOUpZTjZNemhUcHFWTFB5RnBHSzdjcXFGbEV5dkF0c05ra2pEYWVFUVc1UzhlQ3gwODVkaFRhdg=="});
+  http.Response response = await http.get(apiUrl, headers: {"Authorization": "<Secret Code>"});
   return json.decode(response.body);
 
 }
